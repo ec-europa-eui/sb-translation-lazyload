0023fb92e26a61b2109beeb6a1382655b18fb1e5
VENUGOPALAN Uma (DIGIT-EXT) <Uma.VENUGOPALAN@ext.ec.europa.eu>
2022-09-28 07:16:32 +0200

    Added new modules and mapped to lazy load translations

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+     { path: '', redirectTo: 'screen/home', pathMatch: 'full' },
+     { path: 'screen/module1', loadChildren: () => import('./features/module1/module1.module').then(m => m.Module) },
+     { path: 'screen/module2', loadChildren: () => import('./features/module2/module2.module').then(m => m.Module) }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes),
+    ],
+})
+export class AppRoutingModule {}
